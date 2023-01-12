999fcf20d019f338723807d255e2869dae543e58
Poted <o.jpkmc@gmail.com>
2023-01-12 12:02:57 +0100

    pinkUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SuperHero } from './models/super-hero';
 import { SuperHeroService } from './services/super-hero.service';
 
@@ -12,6 +12,12 @@ export class AppComponent {
   title = 'AngularCRUD';
   heroes: SuperHero[] = [];
   heroToEdit?: SuperHero;
+  mapLink: string[] = 
+  [ 'https://goo.gl/maps/8VygmjnFaYUtWSeE7',
+    'https://goo.gl/maps/WwHHbMMusJc3nuTP8', 
+    "https://goo.gl/maps/peQR4satFCXxWmvr6" 
+  ];
+  heroSearch?: string = 'xD';
   
   constructor(private superHeroService: SuperHeroService) {}
 
@@ -31,6 +37,10 @@ export class AppComponent {
 
   editHero(hero: SuperHero){
     this.heroToEdit = hero;
+  }
+
+  searchForHero(hero: SuperHero){
+    return this.heroSearch = `https://www.google.com/search?q=${hero.name}&ie=UTF-8&oe=UTF-8`;
   }
 
 }
